f19f3d1d79f767e7ee946066878aad6f33d1f594
SarkNeha <snehassarker2006@gmail.com>
2026-03-15 20:36:53 +0100

    Created Flask application file app.py

---
+++ apps.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from models import Patient
+from hospital_data import add_patient, get_patients, get_recent
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    patients = get_patients()
+    recent = get_recent()
+    return render_template("index.html", patients=patients, recent=recent)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_patient_page():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        age = request.form["age"]
+        illness = request.form["illness"]
+
+        new_patient = Patient(name, age, illness)
+        add_patient(new_patient)
+
+        return redirect("/")
+
+    return render_template("add_patient.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
